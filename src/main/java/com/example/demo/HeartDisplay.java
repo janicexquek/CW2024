59651a0c16e7fb110984d2706154dd775765acb1
jq <ziying8556@gmail.com>
2024-11-17 17:17:42 +0800

    Update new heart image

--- src/main/java/com/example/demo/HeartDisplay.java
+++ src/main/java/com/example/demo/HeartDisplay.java
@@ -6,14 +6,14 @@ import javafx.scene.layout.HBox;
 
 public class HeartDisplay {
 	
-	private static final String HEART_IMAGE_NAME = "/com/example/demo/images/heart.png";
+	private static final String HEART_IMAGE_NAME = "/com/example/demo/images/heart1.png";
 	private static final int HEART_HEIGHT = 50;
 	private static final int INDEX_OF_FIRST_ITEM = 0;
 	private HBox container;
 	private double containerXPosition;
 	private double containerYPosition;
 	private int numberOfHeartsToDisplay;
-	
+
 	public HeartDisplay(double xPosition, double yPosition, int heartsToDisplay) {
 		this.containerXPosition = xPosition;
 		this.containerYPosition = yPosition;
@@ -42,7 +42,7 @@ public class HeartDisplay {
 		if (!container.getChildren().isEmpty())
 			container.getChildren().remove(INDEX_OF_FIRST_ITEM);
 	}
-	
+
 	public HBox getContainer() {
 		return container;
 	}
