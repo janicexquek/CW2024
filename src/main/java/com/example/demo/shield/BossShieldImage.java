3f084c1e1d03882de5e4173579f7f175ecdf0220
jq <ziying8556@gmail.com>
2024-12-03 19:30:47 +0800

    Updated javadoc to package "shield"

--- src/main/java/com/example/demo/shield/BossShieldImage.java
+++ src/main/java/com/example/demo/shield/BossShieldImage.java
@@ -3,26 +3,43 @@ package com.example.demo.shield;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
+import java.util.Objects;
+
+/**
+ * Class representing the boss shield image in the game.
+ * Manages the display and visibility of the boss shield.
+ */
 public class BossShieldImage extends ImageView {
 
-	private static final int SHIELD_SIZE = 200;
+    private static final int SHIELD_SIZE = 200;
 
-	public BossShieldImage(double xPosition, double yPosition) {
-		this.setLayoutX(xPosition);
-		this.setLayoutY(yPosition);
-		Image shieldImg = new Image(getClass().getResource("/com/example/demo/images/shield.png").toExternalForm());
-		this.setImage(shieldImg);
-		this.setVisible(false);
-		this.setFitHeight(SHIELD_SIZE);
-		this.setFitWidth(SHIELD_SIZE);
-	}
+    /**
+     * Constructor for BossShieldImage.
+     *
+     * @param xPosition the initial X position of the boss shield
+     * @param yPosition the initial Y position of the boss shield
+     */
+    public BossShieldImage(double xPosition, double yPosition) {
+        this.setLayoutX(xPosition);
+        this.setLayoutY(yPosition);
+        Image shieldImg = new Image(Objects.requireNonNull(getClass().getResource("/com/example/demo/images/shield.png")).toExternalForm());
+        this.setImage(shieldImg);
+        this.setVisible(false);
+        this.setFitHeight(SHIELD_SIZE);
+        this.setFitWidth(SHIELD_SIZE);
+    }
 
-	public void showShield() {
-		this.setVisible(true);
-	}
+    /**
+     * Shows the boss shield by setting its visibility to true.
+     */
+    public void showShield() {
+        this.setVisible(true);
+    }
 
-	public void hideShield() {
-		this.setVisible(false);
-	}
-
+    /**
+     * Hides the boss shield by setting its visibility to false.
+     */
+    public void hideShield() {
+        this.setVisible(false);
+    }
 }
