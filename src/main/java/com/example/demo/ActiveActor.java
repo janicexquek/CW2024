60513d80410cd86e4f85784459b059abc72d5a6c
jq <ziying8556@gmail.com>
2024-12-03 19:43:13 +0800

    Updated javadoc to (ActiveActor, ActiveActorDestructible & Destructible)

--- src/main/java/com/example/demo/ActiveActor.java
+++ src/main/java/com/example/demo/ActiveActor.java
@@ -2,27 +2,54 @@ package com.example.demo;
 
 import javafx.scene.image.*;
 
+import java.util.Objects;
+
+/**
+ * Abstract class representing an active actor in the game.
+ * Provides basic structure and behavior for all active actors.
+ */
 public abstract class ActiveActor extends ImageView {
-	
-	private static final String IMAGE_LOCATION = "/com/example/demo/images/";
 
-	public ActiveActor(String imageName, int imageHeight, double initialXPos, double initialYPos) {
-		//this.setImage(new Image(IMAGE_LOCATION + imageName));
-		this.setImage(new Image(getClass().getResource(IMAGE_LOCATION + imageName).toExternalForm()));
-		this.setLayoutX(initialXPos);
-		this.setLayoutY(initialYPos);
-		this.setFitHeight(imageHeight);
-		this.setPreserveRatio(true);
-	}
+    private static final String IMAGE_LOCATION = "/com/example/demo/images/";
 
-	public abstract void updatePosition();
+    /**
+     * Constructor for ActiveActor.
+     *
+     * @param imageName the name of the image representing the actor
+     * @param imageHeight the height of the image representing the actor
+     * @param initialXPos the initial X position of the actor
+     * @param initialYPos the initial Y position of the actor
+     */
+    public ActiveActor(String imageName, int imageHeight, double initialXPos, double initialYPos) {
+        // Set the image for the actor using the provided image name
+        this.setImage(new Image(Objects.requireNonNull(getClass().getResource(IMAGE_LOCATION + imageName)).toExternalForm()));
+        this.setLayoutX(initialXPos);
+        this.setLayoutY(initialYPos);
+        this.setFitHeight(imageHeight);
+        this.setPreserveRatio(true);
+    }
 
-	protected void moveHorizontally(double horizontalMove) {
-		this.setTranslateX(getTranslateX() + horizontalMove);
-	}
+    /**
+     * Updates the position of the active actor.
+     * This method must be implemented by subclasses.
+     */
+    public abstract void updatePosition();
 
-	protected void moveVertically(double verticalMove) {
-		this.setTranslateY(getTranslateY() + verticalMove);
-	}
+    /**
+     * Moves the actor horizontally by a specified amount.
+     *
+     * @param horizontalMove the amount to move the actor horizontally
+     */
+    protected void moveHorizontally(double horizontalMove) {
+        this.setTranslateX(getTranslateX() + horizontalMove);
+    }
 
+    /**
+     * Moves the actor vertically by a specified amount.
+     *
+     * @param verticalMove the amount to move the actor vertically
+     */
+    protected void moveVertically(double verticalMove) {
+        this.setTranslateY(getTranslateY() + verticalMove);
+    }
 }
--- src/main/java/com/example/demo/ActiveActorDestructible.java
+++ src/main/java/com/example/demo/ActiveActorDestructible.java
@@ -1,51 +1,86 @@
 package com.example.demo;
 
+/**
+ * Abstract class representing a destructible active actor in the game.
+ * Extends ActiveActor and implements Destructible interface.
+ * Provides additional functionality for handling destruction.
+ */
 public abstract class ActiveActorDestructible extends ActiveActor implements Destructible {
 
-	private boolean isDestroyed;
+    private boolean isDestroyed;
 
-	public ActiveActorDestructible(String imageName, int imageHeight, double initialXPos, double initialYPos) {
-		super(imageName, imageHeight, initialXPos, initialYPos);
-		isDestroyed = false;
-	}
-	// Add this enum to represent the cause of destruction
-	public enum DestroyedBy {
-		NONE, USER_PROJECTILE, COLLISION_WITH_USER, PENETRATION
-	}
+    /**
+     * Constructor for ActiveActorDestructible.
+     *
+     * @param imageName the name of the image representing the actor
+     * @param imageHeight the height of the image representing the actor
+     * @param initialXPos the initial X position of the actor
+     * @param initialYPos the initial Y position of the actor
+     */
+    public ActiveActorDestructible(String imageName, int imageHeight, double initialXPos, double initialYPos) {
+        super(imageName, imageHeight, initialXPos, initialYPos);
+        isDestroyed = false;
+    }
 
-	// Add this field to store the cause
-	private DestroyedBy destroyedBy = DestroyedBy.NONE;
+    /**
+     * Enum representing the cause of destruction.
+     */
+    public enum DestroyedBy {
+        NONE, USER_PROJECTILE, COLLISION_WITH_USER, PENETRATION
+    }
 
-	@Override
-	public abstract void updatePosition();
+    private DestroyedBy destroyedBy = DestroyedBy.NONE;
 
-	public abstract void updateActor();
+    @Override
+    public abstract void updatePosition();
 
-	@Override
-	public abstract void takeDamage();
+    public abstract void updateActor();
 
-	// Modify the destroy method to set the default cause if not already set
-	@Override
-	public void destroy() {
-		if (destroyedBy == DestroyedBy.NONE) {
-			destroyedBy = DestroyedBy.PENETRATION; // Default cause if not set
-		}
-		setDestroyed(true);
-	}
-	// Add getter and setter for destroyedBy
-	public DestroyedBy getDestroyedBy() {
-		return destroyedBy;
-	}
+    @Override
+    public abstract void takeDamage();
 
-	public void setDestroyedBy(DestroyedBy destroyedBy) {
-		this.destroyedBy = destroyedBy;
-	}
-	protected void setDestroyed(boolean isDestroyed) {
-		this.isDestroyed = isDestroyed;
-	}
+    /**
+     * Destroys the actor and sets the default cause if not already set.
+     */
+    @Override
+    public void destroy() {
+        if (destroyedBy == DestroyedBy.NONE) {
+            destroyedBy = DestroyedBy.PENETRATION; // Default cause if not set
+        }
+        setDestroyed();
+    }
 
-	public boolean isDestroyed() {
-		return isDestroyed;
-	}
-	
+    /**
+     * Gets the cause of destruction.
+     *
+     * @return the cause of destruction
+     */
+    public DestroyedBy getDestroyedBy() {
+        return destroyedBy;
+    }
+
+    /**
+     * Sets the cause of destruction.
+     *
+     * @param destroyedBy the cause of destruction
+     */
+    public void setDestroyedBy(DestroyedBy destroyedBy) {
+        this.destroyedBy = destroyedBy;
+    }
+
+    /**
+     * Marks the actor as destroyed.
+     */
+    protected void setDestroyed() {
+        this.isDestroyed = true;
+    }
+
+    /**
+     * Checks if the actor is destroyed.
+     *
+     * @return true if the actor is destroyed, false otherwise
+     */
+    public boolean isDestroyed() {
+        return isDestroyed;
+    }
 }
